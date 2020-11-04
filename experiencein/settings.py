1dc59803bb4d0289139f69e423e83befeb60eea9
Vanessa Formiga <vanessandrade16@gmail.com>
2020-11-04 00:09:20 -0200

    adicionado restframework a lista de apps

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'perfis',
-    'usuarios'
+    'usuarios',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
