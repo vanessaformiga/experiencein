a7dfb4bb4bc6d86c04582dd6aa666284d4a281f6
Vanessa Formiga <vanessandrade16@gmail.com>
2020-11-05 10:48:45 -0200

    modificando arquivos urls.py

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,11 +1,22 @@
 #novo arquivo experiencein/perfis/urls.py
 
-from django.urls import path
+from django.urls import path, include
 from perfis import views
+from rest_framework.authtoken import views as auth_views
+
+from  rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('perfis', views.PerfilViewSet)
+
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
-	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar'),
-	path('convite/<int:convite_id>/aceitar', views.aceitar, name='aceitar')
+    path('', include(router.urls)),
+	path('perfil/',views.get_meu_perfil, name='perfil'),
+	path('convites/', views.get_convites, name='convites'),
+	path('convites/convidar/<int:perfil_id>', views.convidar, name='convidar'),
+	path('convites/aceitar/<int:convite_id>', views.aceitar, name='aceitar'),
+	path('login/', auth_views.obtain_auth_token, name='login')
 ]
+
